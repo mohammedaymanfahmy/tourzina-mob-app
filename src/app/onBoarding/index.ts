602d9b093750b16bdbff7d572b8a878be9e940d3
Mohamed-Ayman-Fahmy <muhamed.ayman.fahmy@gmail.com>
2025-12-17 18:01:54 +0200

    fix structure

---
+++ src/app/onBoarding/index.ts
@@ -0,0 +1,3 @@
+export { default as Onboarding } from './screens/Onboarding';
+export { default as GetStarted } from './screens/GetStarted';
+export { default as LanguageSelection } from './screens/LanguageSelection';
--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,5 +1,1 @@
 export { default as Example } from './Example/Example';
-export { default as Onboarding } from '../app/onBoarding/screens/Onboarding';
-export { default as Startup } from './Startup/Startup';
-export { default as GetStarted } from '../app/onBoarding/screens/GetStarted';
-export { default as LanguageSelection } from '../app/onBoarding/screens/LanguageSelection';
